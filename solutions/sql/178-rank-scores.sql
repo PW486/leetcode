2a09e4c821420f640ee03342dc9eac3158e7909d
Donggeon Lim <ooqwe486@gmail.com>
2020-02-21 08:45:57 +0000

    Add 178-rank-scores.sql

---
+++ solutions/sql/178-rank-scores.sql
@@ -0,0 +1,19 @@
+SELECT
+  Score,
+  (
+    SELECT
+      count(*)
+    FROM
+      (
+        SELECT
+          DISTINCT Score s
+        FROM
+          Scores
+      ) tmp
+    WHERE
+      s >= Score
+  ) Rank
+FROM
+  Scores
+ORDER BY
+  Score DESC
